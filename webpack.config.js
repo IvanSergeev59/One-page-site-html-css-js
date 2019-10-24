c27f3605bf3e4d5db5ce6b59620767510e917213
Ivan Sergeev <ahrejlok2007@ya.ru>
2019-10-25 00:17:55 +0500

    fist

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const webpack = require('webpack')
 const path = require('path');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash'); // добавили плагин
@@ -19,24 +22,53 @@ module.exports = {
           loader: "babel-loader"
         }
       },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
+        },
+     
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+             'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
+             {
+                 loader: 'image-webpack-loader',
+                 options: { // ... }
+             }
+            }
+        ]
+        },
   {
-        test: /\.css$/,
-        use:  [MiniCssExtractPlugin.loader, 'css-loader']
-       }
+    test: /\.css$/,
+    use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
+       },
+       
     ]
   },
   plugins: [ 
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+ }),
+ 
     new MiniCssExtractPlugin({
         filename: 'style.[contenthash].css'
     }),
+   
+  new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+              preset: ['default'],
+      },
+      canPrint: true
+ }),
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash(),
-    new webpack.DefinePlugin({
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-   })
-  ]
+
+    new WebpackMd5Hash()
+  ],
+  
 };
